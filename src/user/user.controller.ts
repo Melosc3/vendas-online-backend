abcef86a20f6369cfce0249e25a8b624e6153833
fernando <nandomelo@hotmail.com>
2023-09-27 13:23:26 -0300

    Backend usando Postman e TS

---
+++ src/user/user.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post, Get } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { User } from './interfaces/user.interface';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Post()
+    async createUser(@Body() createUser: CreateUserDto): Promise<User> {
+        return this.userService.createUser(createUser);
+    }
+
+    @Get()
+    async getAllUser(): Promise<User[]> {
+        return this.userService.getAllUser();
+    }
+}
